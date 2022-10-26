7c88e014aba19cbb02e39ad2c31f4b78bb391ae5
RicardoCaselati <ricardocaselati@gmail.com>
2022-10-26 17:01:27 -0300

    req.03 - ok

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+// const authService = require('../services/auth.service');
+const { User } = require('../models');
+const { createToken } = require('../utils/jwt.util');
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === '' || password === '') {
+        return res.status(400).json({ message: 'Some required fields are missing' });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user || user.password !== password) {
+        return res.status(400).json({ message: 'Invalid fields' });
+    }
+
+    // const token = await authService.validateLogin({ email, password });
+
+    // const token = process.env.JWT_SECRET; 
+    const token = await createToken(email);
+    res.status(200).json({ token });
+};
+
+module.exports = { login };
--- src/models/blogPost.js
+++ src/models/blogPost.js
@@ -1,11 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define('BlogPost', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      primaryKey: true
-    },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     published: DataTypes.DATE,
