27a1a3723d7d7b04367524afddbf793d73d1a51a
RicardoCaselati <ricardocaselati@gmail.com>
2022-10-26 08:56:39 -0300

    req.01 - ok

---
+++ src/models/blogPost.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    tablename: 'blog_posts',
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'users',
+    })
+  };
+
+  return BlogPost;
+};
---
+++ src/models/category.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    name: DataTypes.STRING,
+  }, {
+    tablename: 'categories',
+    underscored: true,
+  });
+
+  return Category;
+};
